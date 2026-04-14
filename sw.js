513bec84272ba4b2ebc47d6d85220410a2dfbf5a
zaykhant <72433511+zaykhant@users.noreply.github.com>
2026-04-14 13:42:34 +0630

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'baydin-v2';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
